054f2392cfd81bf072a7fcc00e9f51d7e3307a6a
Albert Meltzer <albert.meltzer@walmart.com>
2021-07-13 00:11:43 -0700

    LowercasePatternMatch: find more invalid patterns
    This includes pattern extractors, not just standalone vals.

--- src/main/scala/org/scalastyle/scalariform/LowercasePatternMatchChecker.scala
+++ src/main/scala/org/scalastyle/scalariform/LowercasePatternMatchChecker.scala
@@ -17,7 +17,7 @@
 package org.scalastyle.scalariform
 
 import _root_.scalariform.lexer.Token
-import _root_.scalariform.lexer.Tokens.VARID
+import _root_.scalariform.lexer.Tokens._ // scalastyle:ignore underscore.import
 import _root_.scalariform.parser.CasePattern
 import _root_.scalariform.parser.CompilationUnit
 import org.scalastyle.PositionError
@@ -31,18 +31,26 @@ class LowercasePatternMatchChecker extends ScalariformChecker {
   final def verify(ast: CompilationUnit): List[ScalastyleError] = {
     val it = for {
       f <- visit(map)(ast.immediateChildren.head)
-      if matches(f)
-    } yield PositionError(f.pattern.firstToken.offset)
+      t <- matches(f.pattern.tokens, Nil)
+    } yield PositionError(t.offset)
 
     it
   }
 
-  private def matches(t: CasePattern) = {
-    t.pattern.tokens match {
-      case List(t: Token) => t.tokenType == VARID && t.text.length() > 0 && t.text(0).isLower
-      case _              => false
+  @scala.annotation.tailrec
+  private def matches(tokens: List[Token], res: List[Token]): List[Token] =
+    tokens.headOption match {
+      case None => res
+      case Some(t @ Token(VARID, text, _, _)) if text.headOption.exists(_.isLower) =>
+        val tail = tokens.tail
+        tail.headOption match {
+          case Some(Token(COMMA | RPAREN | AT, _, _, _)) =>
+            matches(tail.tail, t :: res)
+          case None => t :: res
+          case _    => matches(tail.tail, res)
+        }
+      case _ => matches(tokens.tail, res)
     }
-  }
 
   private def map(t: CasePattern): List[CasePattern] =
     List(t) ::: visit(map)(t.pattern) ::: visit(map)(t.guardOption)
